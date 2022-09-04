57fceddb9e52f5639327e1644c41dbbd2327a287
hamed <sahami.hamed@gmail.com>
2022-09-04 14:31:02 +0430

    Setup first release

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SidebarComponent} from "./sidebar/sidebar.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
+import {RouterModule} from "@angular/router";
+import {APP_EXTRA_OPTIONS, APP_ROUTES} from "./app.routes";
+import {FormsModule} from "@angular/forms";
+import {CUSTOMER_ROUTES} from "./customer/customer.routes";
+import {HomeComponent} from "./home/home.component";
+import {CustomerModule} from "./customer/customer.module";
+import {HttpClientModule} from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,HttpClientModule,   FormsModule,
+    RouterModule.forRoot([...APP_ROUTES], {...APP_EXTRA_OPTIONS}),
+    // RouterModule.forRoot([...CUSTOMER_ROUTES], {...APP_EXTRA_OPTIONS}),
   ],
-  providers: [],
+  providers: [CustomerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
